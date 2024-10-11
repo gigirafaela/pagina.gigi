164eaa42e72f3abb629065adc02802d2eb60441d
gigirafaela <144805530+gigirafaela@users.noreply.github.com>
2024-10-10 23:17:07 -0300

    Add files via upload

--- FLASHCARD/app.js
+++ FLASHCARD/app.js
@@ -1,3 +1,32 @@
 function criaCartao (categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
-}
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    
+    console.log(container)
+    
+    cartao.className = 'cartao'
+    
+    console.log(cartao)
+
+    cartao.innerHTML = `  
+        <div class="cartao__conteudo">
+            <h3> ${categoria}</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p> ${pergunta}</p>
+            </div>                        
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
+`
+
+    let respostaEstavisivel = false
+    function viraCartão() {
+        respostaEstavisivel = !respostaEstavisivel
+        cartao.classList.toggle('active', respostaEstavisivel)
+    }
+    cartao.addEventListener('click', viraCartão)
+
+    container.appendChild(cartao)
+    
+ }
